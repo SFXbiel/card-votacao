7daf8bda2b434bc826aa26efdcd2654e7a5517e2
SFXbiel <gabrielsousarangel5.az2@gmail.com>
2023-01-14 05:05:27 -0300

    Concertando bug
    Concertando bug

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let counter = 0;
 function positivo() {
-    counter = counter + 1;
+    counter += 1;
     document.getElementById("fofo").innerHTML = counter + " Gostei";
 }
 
 let noCounter = 0;
 function negativo() {
     noCounter = noCounter + 1;
-    document.getElementById("naofofo").innerHTML = noCounter + " Não gostei";
+    document.getElementById("naofofo").innerHTML = noCounter + " Não Gostei";
 }
 
 function reset() {
-    document.getElementById("reset").value = " ";
-    document.getElementById("fofo").innerHTML = "0";
-    document.getElementById("naofofo").innerHTML = "0";
+    document.getElementById("reset").value = "0";
+    document.getElementById("fofo").innerHTML = "0 Gostei";
+    document.getElementById("naofofo").innerHTML = "0 Não Gostei";
+
+    noCounter = 0
+    counter = 0
 }
